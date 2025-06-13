05c9d2349629093634ec9f5578641a82ecc1f867
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-06-13 18:43:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pdf-metadata-cache').then(cache => {
+      return cache.addAll(['.', 'index.html', 'manifest.json']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
