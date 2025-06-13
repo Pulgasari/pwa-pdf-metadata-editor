9c1a6d1d34b053727f53e752e7bcc5032cedb6a9
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-06-13 19:49:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,34 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('pdf-metadata-cache').then(cache => {
-      //return cache.addAll(['.', 'index.html', 'manifest.json']);
-      return cache.addAll(['manifest.json']);
-    })
-  );
+self.addEventListener('install', (event) => {
+  // Optional: Cache leeren bei Installation
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
+
+self.addEventListener('fetch', (event) => {
+  // Netzwerkanfragen ohne Caching
+  event.respondWith(fetch(event.request));
+});
+
+//self.addEventListener('install', event => {
+//  event.waitUntil(
+//    caches.open('pdf-metadata-cache').then(cache => {
+//      //return cache.addAll(['.', 'index.html', 'manifest.json']);
+//      return cache.addAll(['manifest.json']);
+//    })
+//  );
+//  self.skipWaiting();
+//});
+//
+//self.addEventListener('fetch', event => {
+//  event.respondWith(
+//    caches.match(event.request).then(resp => resp || fetch(event.request))
+//  );
+//});
