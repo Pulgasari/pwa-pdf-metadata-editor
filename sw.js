b2b42152d703d6cc1bbd760e80395410b411be78
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-06-13 19:44:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('pdf-metadata-cache').then(cache => {
-      return cache.addAll(['.', 'index.html', 'manifest.json']);
+      //return cache.addAll(['.', 'index.html', 'manifest.json']);
+      return cache.addAll(['manifest.json']);
     })
   );
   self.skipWaiting();
